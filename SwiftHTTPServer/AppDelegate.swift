8ab04e807e814b55e40769504833570a940ffbae
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-07-05 13:36:25 +0200

    common changes

--- SwiftHTTPServer/AppDelegate.swift
+++ SwiftHTTPServer/AppDelegate.swift
@@ -9,21 +9,35 @@
 import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-                            
+    
     @IBOutlet var window: NSWindow
-
-
+    
+    
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
-        // Insert code here to initialize your application
-        SwiftHTTPServer.server.start(3000, callback: {error, server in
-            NSLog("%@", server)
-        })
-    }
 
+        let server = SwiftHTTPServer.server
+        
+        server.get("/hello", callback: {req, res in
+            
+            return true;
+            }).get("/world", callback: [
+                { req, res in
+                    
+                    return true
+                }, {req, res in
+                    
+                    return true
+                }]
+            ).start(3000, callback: {err, server in
+                NSLog("%@", server)
+                })
+        
+    }
+    
     func applicationWillTerminate(aNotification: NSNotification?) {
         // Insert code here to tear down your application
     }
-
-
+    
+    
 }
 
--- SwiftHTTPServer/SwiftHTTPServer.swift
+++ SwiftHTTPServer/SwiftHTTPServer.swift
@@ -8,8 +8,17 @@
 
 import Cocoa
 
-class SwiftHTTPServer: NSObject {
+class SwiftHTTPReq: NSObject {
 
+}
+
+class SwiftHTTPRes: NSObject {
+    
+}
+
+class SwiftHTTPServer: NSObject {
+    var routes = Dictionary<String, Array<(SwiftHTTPReq, SwiftHTTPRes)-> Bool >>()
+    
     class var server: SwiftHTTPServer {
         struct Singleton {
             static let instance = SwiftHTTPServer()
@@ -17,8 +26,20 @@ class SwiftHTTPServer: NSObject {
         return Singleton.instance
     }
     
+    func get(route:String, callback: Array<(SwiftHTTPReq, SwiftHTTPRes)-> Bool >) -> SwiftHTTPServer {
+
+        return self
+    }
     
-    func start(port:Int, callback: (NSError?, SwiftHTTPServer) -> Void) -> Void {
-        callback(nil, self);
+    func get(route:String, callback:(SwiftHTTPReq, SwiftHTTPRes)-> Bool) -> SwiftHTTPServer {
+        if (routes[route]) {
+
+        }
+        return self
+    }
+    
+    func start(port:Int, callback: (NSError?, SwiftHTTPServer) -> Void) -> SwiftHTTPServer {
+        callback(nil, self)
+        return self
     }
 }
