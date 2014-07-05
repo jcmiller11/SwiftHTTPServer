e316c4374bc6e9999f944ae404ab403ea2617436
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-07-05 16:14:07 +0200

    added objc linking

---
+++ SwiftHTTPServer/BridgeTest.m
@@ -0,0 +1,9 @@
+//
+//  BridgeTest.m
+//  SwiftHTTPServer
+//
+//  Created by Marek Kotewicz on 05.07.2014.
+//  Copyright (c) 2014 Marek Kotewicz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
