0a59af6549e950cd9a5321b82e03421ce0cc8821
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-07-05 20:12:07 +0200

    simple serving files

---
+++ SwiftHTTPServer/SwiftHTTPStatic.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftHTTPStatic.swift
+//  SwiftHTTPServer
+//
+//  Created by Marek Kotewicz on 05.07.2014.
+//  Copyright (c) 2014 Marek Kotewicz. All rights reserved.
+//
+
+import Cocoa
+
+class SwiftHTTPStatic: NSObject {
+
+    
+    func servePublicFiles(path:String, server:SwiftHTTPServer)->Void{
+        var arr = NSFileManager.defaultManager().directoryContentsAtPath(path);
+        for i : AnyObject in arr {
+            var file = i as String
+            server.get("" + file, callback: {req, res in
+                var body = NSString.stringWithContentsOfFile(path + file, encoding: NSUTF8StringEncoding, error: nil)
+                res.send(body)
+                return false
+            })
+        }
+    }
+}
